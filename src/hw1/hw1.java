ba707992e013cdbfb56c70bd5143d7b03bd60b20
Peter Chen <maybefamiler@gmail.com>
2025-03-14 16:15:39 +0800

    first commit

---
+++ src/hw1/hw1.java
@@ -0,0 +1,40 @@
+package hw1;
+
+public class hw1 {
+	public static void main(String[] args) {
+//		作業一
+//		int sum = 6 + 12;
+//		System.out.print(sum);
+//		作業二		
+//		int egg = 0;
+//		int da = egg / 12;
+//		int k = egg % 12;
+//		System.out.print("總共" + da + "打" + k + "顆");
+//		作業三
+//		int a = 256559 ;
+//		int day = (a / 86400 );
+//		int hr = (a - day * 86400) / 3600;
+//		int min = ((a - day * 86400) - hr * 3600)/ 60;
+//		int s = ((a - day * 86400) - hr * 60)% 60;		
+//		System.out.print(day + "天" + hr + "小時" + min + "分" + s + "秒");
+//		作業四
+//		final double n = 3.1415;
+//		int r= 5;
+//		double area = n * Math.pow(r, 2);
+//		double circumference = 2 * n * r;
+//		System.out.println("面積為:" + area + "周長為:" + circumference);
+//		作業五
+//		double p = 1500000;
+//		double r = 0.02;
+//		int t = 10;
+//		double A = p * Math.pow(1 + r, t);
+//		System.out.printf("10 年後本金加利息共有 %.2f 元%n", A);
+//		作業六
+		int num1 = 5;
+		char num2 = '5';
+		String num3 = "5";
+		System.out.println(num1 + num1); // 兩個數據類型都是 int 所以會是 5 + 5 = 10
+		System.out.println(num1 + num2); // num2 是字符 '5'，其 Unicode 值為 53 所以答案是 5 + 53 = 58
+		System.out.println(num1 + num3); // num3 是字串 所以會是 "5"跟5拼接 = 55
+	}
+}
---
+++ src/hw2/hw2.java
@@ -0,0 +1,66 @@
+package hw2;
+
+public class hw2 {
+	public static void main(String[] args) {
+		//作業一
+		int sum = 0;
+		for (int i = 1; i <=1000; i++) {
+			if(i % 2 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+//		作業二
+		int sum2 = 1;
+		for(int i = 1; i <= 10; i++) {
+			sum2 *= i;
+		}
+		System.out.println(sum2);
+//		作業三
+		int sum3 = 1;
+		int i = 1;
+		while(i <= 10) {
+			
+			sum3 *= i;
+			i++;
+		}
+		System.out.println(sum3);
+//		作業四
+		for (int o = 1; o <= 10; o++) {
+			
+			System.out.print((int)Math.pow(o, 2));
+			System.out.print(' ');
+		}
+		System.out.println();
+		
+//		作業五
+		int sum4 = 0;
+		for (int o = 1; o <= 49; o++) {
+			if(o / 10 != 4  && o % 10 != 4 ) {
+				System.out.print(o);
+				sum4 += 1;
+			}
+		}
+		System.out.println();
+		System.out.println(sum4);
+//		作業六
+		for (int o = 10; o >= 1; o--) {
+			for (int y = 1; y <= o; y++) {
+				System.out.print(y + " ");
+			}
+			System.out.println();
+		}
+//		作業七
+		char a = 64;
+		for (int o = 1; o <= 6; o++) {
+			for (int j = 0; j < o; j++) {
+//				char b = (char)(a + o);
+//				System.out.print(b);
+				System.out.printf("%c", a + o);
+			}
+			System.out.println();
+		}
+		
+	}
+		
+}
