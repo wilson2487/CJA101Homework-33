5d625dd97ceec18b2374b251fef1e0eff5a7d6bf
Peter Chen <maybefamiler@gmail.com>
2025-03-17 14:44:51 +0800

    hw3 second

--- src/hw3/hw3_3.java
+++ src/hw3/hw3_3.java
@@ -8,31 +8,40 @@ import java.util.Scanner;
 //厭哪個數字,請您設計一隻程式,讓阿文可以輸入他不想要的數字(1~9),畫面會顯示他可以選擇
 //的號碼與總數
 public class hw3_3 {
+	
+	
 	public static void lastNumber(int n) {
-		int lastNumber[] = new int[49];
-		for (int o = 1; o <= 48; o++) {
-			if(o % 10 != n && o / 10 != n) {
-				lastNumber[o] = o;
-//				System.out.print(lastNumber[o] +" ");
-			}
-//			else {
-//				lastNumber[o] = ' ';
-//			}
-		}
 		
-		// 1. 過濾非 0 的數字
-		 int[] nonZeroArr = Arrays.stream(lastNumber).filter(num -> num != 0).toArray();
+		int count = 0; //設置總共可選幾個數字
+		int lastNumber[];
+		if (n > 4) {
+			lastNumber =  new int[44];
+		}
+		else {
+			lastNumber = new int[35];
+		}
+		for (int o = 1; o <= 49; o++) {
+			if(o % 10 != n && o / 10 != n) {
+				lastNumber[count] = o;
+				System.out.print(lastNumber[count] +" ");
+				count++ ; //每選到數字一次就會加一
+			}
+		}
+		System.out.println();
+		System.out.print("總共有"+count+"數字可選 " + "  ");
+//		 1. 過濾非 0 的數字
+//		 int[] nonZeroArr = Arrays.stream(lastNumber).filter(num -> num != 0).toArray();
 		 
 		// 2. Fisher-Yates Shuffle 洗牌法隨機打亂陣列
 		 Random random = new Random();
-		 	for (int i = nonZeroArr.length - 1; i > 0; i--) {
+		 	for (int i = lastNumber.length - 1; i > 0; i--) {
 	            int j = random.nextInt(i + 1); // 隨機交換 i 和 j
-	            int temp = nonZeroArr[i];
-	            nonZeroArr[i] = nonZeroArr[j];
-	            nonZeroArr[j] = temp;
+	            int temp = lastNumber[i];
+	            lastNumber[i] = lastNumber[j];
+	            lastNumber[j] = temp;
 	        }
-	        // 3. 取前 6 個數字
-	        int[] selectedNumbers = Arrays.copyOfRange(nonZeroArr, 0, 6);
+	        // 3. 取前 6 個數字並複製一個新的陣列避免修改原始數據
+	        int[] selectedNumbers = Arrays.copyOfRange(lastNumber, 0, 6);
 
 	        // 4. 印出結果
 	        System.out.println("隨機選取的的數字：" + Arrays.toString(selectedNumbers));
