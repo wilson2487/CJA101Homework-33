df647ac485ae173b350c6deb8720a72dae261961
Peter Chen <maybefamiler@gmail.com>
2025-03-30 11:30:31 +0800

    hw6 上傳

--- src/hw6/CalTest.java
+++ src/hw6/CalTest.java
@@ -3,20 +3,31 @@ package hw6;
 import java.util.Scanner;
 
 public class CalTest {
-	public static void main(String[] args) throws CalException{
+	public static void main(String[] args) throws CalException {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("請輸入一個數");
-		int x = sc.nextInt();
-		System.out.println("請輸入一個數");
-		int y = sc.nextInt();
-		
-		Calculater cal = new Calculater();
-		try {
-			cal.powerXY(x, y);
-			
-		} catch (CalException e) {
-			System.out.println(e.getMessage());
+		if (sc.hasNextInt()) {
+			int x = sc.nextInt();
+			System.out.println("請輸入一個數");
+			if (sc.hasNextInt()) {
+				int y = sc.nextInt();
+				Calculater cal = new Calculater();
+				try {
+					cal.powerXY(x, y);
+
+				} catch (CalException e) {
+					System.out.println(e.getMessage());
+				}
+			} else {
+				String y = sc.nextLine();
+				System.out.println("第二個輸入格式錯誤");
+			}
+
+		} else {
+			String x = sc.nextLine();
+			System.out.println("輸入格式錯誤");
 		}
+
 	}
-	
+
 }
--- src/hw6/Calculater.java
+++ src/hw6/Calculater.java
@@ -18,20 +18,5 @@ public class Calculater {
 		}
 		
 	}
-	public void powerXY(String x, String y) throws CalException{
-		throw new CalException("輸入格式不正確");
-	}
-	public void powerXY(String x, int y) throws CalException{
-		throw new CalException("輸入格式不正確");
-	}
-	public void powerXY(int x, String y) throws CalException{
-		throw new CalException("輸入格式不正確");
-	}
-	public void powerXY(char x, int y) throws CalException{
-		throw new CalException("輸入格式不正確");
-	}
-	public void powerXY(int x, char y) throws CalException{
-		throw new CalException("輸入格式不正確");
-	}
 	
 }
